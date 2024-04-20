34ff49fbb241e0a2f920e0b0fc1f56a112a1a56c
AsafDov <Nathendov@gmail.com>
2024-04-20 22:51:40 +0300

    Still need to write the entry input mechanism

--- EventLogger_Project/EventLogger/models.py
+++ EventLogger_Project/EventLogger/models.py
@@ -5,8 +5,14 @@ class Event(models.Model):
     name = models.CharField(max_length=200, primary_key=True)
     createDate = models.CharField(max_length=50)
 
+    def __str__(self):
+        return f"Event:{self.name}"
+    
+
 class Entry(models.Model):
     event = models.ForeignKey("Event", verbose_name=("Event"), on_delete=models.CASCADE)
     entry = models.TextField()
     entryDate = models.DateTimeField(auto_now=True)
 
+    def __str__(self):
+        return f"{self.entry}"
--- EventLogger_Project/EventLogger/views.py
+++ EventLogger_Project/EventLogger/views.py
@@ -1,6 +1,48 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from .serializers import *
 from .models import *
-
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 
 def home(request):
-    return render(request, "index.html", {"Event":Event, "Entry":Entry})
+    events = Event.objects.all()
+    context = {"Events":events}
+    
+    return render(request, "index.html", context)
+
+
+def handleEventClick(request, *args, **kwargs):
+    if request.method == "GET":
+        print("request is")
+        print(request)
+        print(str(request)[20:26:])
+        print("--------------")
+        eventName = str(request)[20:26:]
+        entries = Entry.objects.filter(event=eventName)
+        context = {"eventEntries":entries}
+        return render(request, "index.html", context)
+
+@api_view(['GET'])
+def getEntry(request):
+    if request.method == 'GET':
+        entry = Entry.objects.all()
+        serializer = EntrySerializer(entry, many=True)
+        return JsonResponse({"entries":serializer.data})
+
+@api_view(['POST'])
+def addEntry(request):
+    if request.method == 'POST':
+        serializer = EntrySerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+# Returning the entries for a specific event
+@api_view(['GET'])
+def getEntries(request, event):
+    if request.method == 'GET':
+        entry = Entry.objects.filter(event=event)
+        serializer = EntrySerializer(entry, many=True)
+        return JsonResponse({"entries":serializer.data})
