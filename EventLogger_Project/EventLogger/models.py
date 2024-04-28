9c10bea7b056f14c02728f696433eba12198ec99
AsafDov <NathenDov@gmail.com>
2024-04-28 15:54:33 +0300

    Added populate model and test folder. Need to keep working on it.

--- EventLogger_Project/EventLogger/models.py
+++ EventLogger_Project/EventLogger/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+import os
 
 class Event(models.Model):
     name = models.CharField(max_length=200, unique=True)
@@ -7,7 +7,17 @@ class Event(models.Model):
     def __str__(self):
         return f"Event:{self.name}"
     
+    def populate(self, path="C:\\Users\\nathe\\Desktop\\Projects\\EventLogger\\Test"):
+        results = []
+        for f in os.listdir(path):
+            if f.endswith('.txt'):
+                print(f)
+                print(os.path.getctime(path + "/" + f))
+                results.append(f)
+        return (results)
 
+event = Event()
+print(event.populate())
 class Entry(models.Model):
     event = models.ForeignKey("Event", verbose_name=("Event"), on_delete=models.CASCADE)
     entry = models.TextField()
