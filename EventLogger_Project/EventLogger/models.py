4113fe749895fbe4313d9ca5dbbbc47ed03423c4
AsafDov <NathenDov@gmail.com>
2024-05-05 15:13:58 +0300

    Fixed load data again

--- EventLogger_Project/EventLogger/models.py
+++ EventLogger_Project/EventLogger/models.py
@@ -17,22 +17,23 @@ class Event(models.Model):
         results = []
         allEvents = Event.objects.all()
         for (root, dirs, file) in os.walk(path):
-            for d in dirs:
-                for f in file:
-                    if '.gcd' in f or '.gcm' in f: #Change to txt for testing
-                        created = time.ctime(os.path.getctime(root + "\\" + d + "\\" + f))
-                        event = {
-                            "name": f,
-                            "createDate":created
-                        }
-                        try:
-                            Event.objects.get(name=event["name"])
-                            results.append(event)
-                            continue
-                        except Event.DoesNotExist:
-                            e = Event(name=event["name"], createDate=event["createDate"] )
-                            e.save()
-                            continue
+            for f in file:
+                if '.gcd' in f or '.gcm' in f or '.txt' in f: #Change to txt for testing
+                    filepath = os.path.join(root, f)
+                    print(filepath)
+                    created = time.ctime(os.path.getctime(root + "\\" + f))
+                    event = {
+                        "name": f,
+                        "createDate":created
+                    }
+                    try:
+                        Event.objects.get(name=event["name"])
+                        continue
+                    except Event.DoesNotExist:
+                        e = Event(name=event["name"], createDate=event["createDate"] )
+                        e.save()
+                        results.append(event)
+                        continue
         return (results)
 
 
